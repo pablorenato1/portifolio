e6b808420d1b83a863b6f456216d114096d417a4
Pablo Renato <pablorenato18@gmail.com>
2025-02-10 10:55:01 -0300

    wip: subindo arquivos com o fetch dos projetos

--- scripts/project.js
+++ scripts/project.js
@@ -1,6 +1,13 @@
 
 const lang = document.documentElement.lang;
 
+const data = fetch("https://raw.githubusercontent.com/pablorenato1/portifolio/refs/heads/fix/structure-projects/projects/project.json")
+    .then(response => {
+    if (!response.ok) {
+        throw new Error("Erro ao carregar o JSON");
+    }
+    return response.json();
+})
+
 function fillThePlaceHolders() {
-    
 }
